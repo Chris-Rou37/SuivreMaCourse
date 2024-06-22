4b0c4de9fb1b827612a5cfd1ff33b75409041dae
‘RoucherChristophe’ <c.roucher@orange.fr>
2024-06-22 18:03:10 +0200

    Création de 'CarteOpenStreetMap' avec 'db_connect.php

---
+++ CarteOpenStreetMAp/db_connect.php
@@ -0,0 +1,21 @@
+<?php
+
+// Tentative de connexion à la base de données MySQL en utilisant l'adresse du serveur (localhost), 
+// le nom d'utilisateur (root) et un mot de passe vide
+$cnx = mysqli_connect("localhost", "root", "");
+
+// Vérification si la connexion a échoué
+if (!$cnx) {
+  // Affichage d'un message d'erreur si la connexion au serveur a échoué
+  echo "Erreur de connexion au serveur";
+}
+
+// Sélection de la base de données nommée 'suividecourse'
+$db = mysqli_select_db($cnx, "suividecourse");
+
+// Vérification si la sélection de la base de données a échoué
+if (!$db) {
+  // Affichage d'un message d'erreur si la connexion à la base de données a échoué
+  echo "Erreur de connexion à la base";
+}
+?>
